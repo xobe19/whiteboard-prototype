3927eb862c25a82335bb3fdc6c9112746a0376b0
Hitesh Lalwani <lwni@protonmail.com>
2024-05-07 09:16:21 +0530

    kafka wip

--- backend/whiteboard_server/whiteboard/consumers.py
+++ backend/whiteboard_server/whiteboard/consumers.py
@@ -1,19 +1,33 @@
 import json
 from channels.generic.websocket import WebsocketConsumer
+from asgiref.sync import async_to_sync
+from utils import start_consumer
 class BoardEventConsumer(WebsocketConsumer):
     def connect(self):
         session = self.scope["session"]
         board = str(self.scope["path"]).split("/")[-1]
         if session.get(board, False):
+            self.board = board
+            async_to_sync(self.channel_layer.group_add)("boards", self.channel_name)
+            start_consumer() 
             self.accept()
         else:
             self.close()
     def disconnect(self, code):
-        pass
+            async_to_sync(self.channel_layer.group_discard)("boards", self.channel_name)
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
         self.send(text_data=json.dumps({
             'message': message
         }))
+    def board_change(self, event):
+        print("fired")
+        board_id = event["board_id"]
+        if self.board != board_id:
+            return
+        self.send(text_data=json.dumps(event))
+    
+    
+
     
--- backend/whiteboard_server/whiteboard/utils.py
+++ backend/whiteboard_server/whiteboard/utils.py
@@ -1,11 +1,28 @@
 from kafka import KafkaProducer, KafkaConsumer
-import time
+from channels.layers import get_channel_layer 
+from asgiref.sync import async_to_sync
+import json
+
 producer = KafkaProducer(bootstrap_servers='localhost:9092', api_version=(2,13,3))
-producer.send('sample', b'hehe')
-producer.flush()
-producer.close()
-    
-consumer = KafkaConsumer("sample", bootstrap_servers='localhost:9092', api_version=(2,13,3), auto_offset_reset='earliest', group_id=None)
-# 
-for msg in consumer:
-  print(msg)
+consumer = KafkaConsumer("whiteboard", bootstrap_servers='localhost:9092', api_version=(2,13,3), auto_offset_reset='earliest', group_id=None)
+channel_layer = get_channel_layer()
+
+def add_to_q(msg):
+    producer.send("whiteboard", json.dumps(msg))
+
+consumer_running = False
+
+def start_consumer():
+    if not consumer_running:
+        consumer_running = True
+    else:
+        return
+    for msg in consumer:
+        data = json.loads(msg["value"])
+        board_id = data["board_id"]    
+        state_delta = data["state_delta"]
+        async_to_sync(channel_layer.group_send)("boards", {
+          "type": "board.change",
+          "board_id": board_id,
+          "state_delta": state_delta
+        })
